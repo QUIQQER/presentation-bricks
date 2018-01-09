4746ceff301568f31a3dd62b185ca3fa6347171f
michael.daniel <michael.danielczok@gmail.com>
2018-01-09 16:43:37 +0100

    feat: Neues Brick / Control: Count up. Die erste Version ist fertig. Es funktioniert noch nicht ideal, aber vorerst reicht. Design und erste Einstellungen sind direkt gebaut.

---
+++ bin/Controls/CountUpBasicEntry.js
@@ -0,0 +1,112 @@
+/**
+ * QUIQQER Count Up Basic
+ *
+ * @author www.pcsg.de (Michael Danielczok)
+ * @module Bricks\Controls\CountUpBasic
+ */
+define('package/quiqqer/presentation-bricks/bin/Controls/CountUpBasicEntry', [
+
+    'qui/QUI',
+    'qui/controls/Control'
+
+], function (QUI, QUIControl) {
+    "use strict";
+
+    return new Class({
+
+        Extends: QUIControl,
+        Type   : 'package/quiqqer/presentation-bricks/bin/Controls/CountUpBasicEntry',
+
+        Binds: [
+            '$onImport',
+            '$count',
+            '$scroll'
+        ],
+
+        options: {
+            duration    : 1000,
+            speed       : 50,
+            delay       : 500,
+            asynchronous: false
+        },
+
+        initialize: function (options) {
+            this.parent(options);
+
+            this.$counter = null;
+            this.start = null;
+            this.end = null;
+            this.countValue = null;
+
+            this.addEvents({
+                onImport: this.$onImport
+            });
+        },
+
+        /**
+         * event : on import
+         */
+        $onImport: function () {
+            this.$counter = this.$Elm.getElement('.countUpBasic-entry-number-counter');
+
+            this.start = this.$counter.get('html').toInt();
+            this.end = this.$counter.getAttribute('data-qui-count').toInt();
+            this.countValue = this.start;
+
+            var winPos = QUI.getScroll().y;
+            var winSize = QUI.getWindowSize().y;
+            var controlPos = this.$counter.getPosition().y;
+            this.breakPoint = controlPos - (winSize / 1.25);
+            this.scrolled = false;
+
+//            console.log('win pos ' + winPos)
+//            console.log('win size ' + winSize)
+//            console.log('control pos ' + controlPos)
+//            console.log('break point ' + breakPoint)
+
+
+
+            if (QUI.getScroll().y > this.breakPoint) {
+                this.$prepareCount();
+
+            }
+
+            QUI.addEvent('scroll', this.$scroll)
+
+        },
+
+        $scroll: function() {
+            if (this.scrolled) {
+                return;
+            }
+            if (QUI.getScroll().y > this.breakPoint) {
+                this.$prepareCount();
+            }
+        },
+
+        $prepareCount: function () {
+            this.scrolled = true;
+
+            var step = Math.ceil(this.end / (this.options.duration / this.options.speed));
+            this.$count(step);
+        },
+
+        $count: function (step) {
+            var self = this;
+
+            (function () {
+                moofx.requestFrame(function () {
+                    self.$counter.set('html', self.countValue);
+                    self.countValue += step;
+
+                    if (self.countValue > self.end) {
+                        self.$counter.set('html', self.end);
+                        return;
+                    }
+
+                    self.$count(step);
+                }.bind(self));
+            }).delay(self.options.speed);
+        }
+    });
+});
