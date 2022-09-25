09865c506ed387bd504fd8c2165ad163613dec19
michael.danielczok <michael@pcsg.de>
2022-09-25 17:22:12 +0200

    feat: New brick added --> BackgroundVideo

---
+++ src/QUI/PresentationBricks/Controls/BackgroundVideo.php
@@ -0,0 +1,137 @@
+<?php
+
+/**
+ * This file contains QUI\PresentationBricks\Controls\BackgroundVideo
+ */
+
+namespace QUI\PresentationBricks\Controls;
+
+use QUI;
+
+/**
+ * Class BackgroundVideo
+ *
+ * @package quiqqer/presentation-bricks
+ */
+class BackgroundVideo extends QUI\Control
+{
+    /**
+     * constructor
+     *
+     * @param array $attributes
+     */
+    public function __construct($attributes = [])
+    {
+        // default options
+        $this->setAttributes([
+            'video'                     => false,
+            'poster'                    => false,
+            'backgroundColor'           => '#333',
+            'shortVideo'                => false,
+            'autoplay'                  => true,
+            'muted'                     => true,
+            'loop'                      => true,
+            'playsinline'               => true,
+            'backgroundVideoBrightness' => 50,
+            'fontColor'                 => '#fff',
+            'contentMaxWidth'           => false,
+            'contentPosition'           => 'center',
+            'openVideoInPopup'          => true,
+            'openVideoInPopupButton'    => 'useDefaultButton'
+        ]);
+
+        parent::__construct($attributes);
+
+        $this->addCSSFile(
+            dirname(__FILE__).'/BackgroundVideo.css'
+        );
+    }
+
+    /**
+     * (non-PHPdoc)
+     *
+     * @see \QUI\Control::create()
+     */
+    public function getBody()
+    {
+        $Engine                    = QUI::getTemplateManager()->getEngine();
+        $backgroundColor           = '#333';
+        $autoplay                  = false;
+        $muted                     = false;
+        $loop                      = false;
+        $playsinline               = false;
+        $backgroundVideoBrightness = 50;
+        $fontColor                 = '#fff';
+        $contentMaxWidth           = false;
+        $openVideoInPopup          = false;
+
+        if ($this->getAttribute('backgroundColor')) {
+            $backgroundColor = $this->getAttribute('backgroundColor');
+        }
+
+        if ($this->getAttribute('autoplay')) {
+            $autoplay = $this->getAttribute('autoplay');
+        }
+
+        if ($this->getAttribute('muted')) {
+            $muted = $this->getAttribute('muted');
+        }
+
+        if ($this->getAttribute('loop')) {
+            $loop = $this->getAttribute('loop');
+        }
+
+        if ($this->getAttribute('playsinline')) {
+            $playsinline = $this->getAttribute('playsinline');
+        }
+
+        if (intval($this->getAttribute('backgroundVideoBrightness')) &&
+            intval($this->getAttribute('backgroundVideoBrightness')) > 0 &&
+            intval($this->getAttribute('backgroundVideoBrightness')) <= 100) {
+            $backgroundVideoBrightness = $this->getAttribute('backgroundVideoBrightness');
+        }
+
+        if ($this->getAttribute('fontColor')) {
+            $fontColor = $this->getAttribute('fontColor');
+        }
+
+        if (intval($this->getAttribute('contentMaxWidth')) &&
+            intval($this->getAttribute('contentMaxWidth')) > 0) {
+            $contentMaxWidth = $this->getAttribute('contentMaxWidth');
+        }
+
+        switch ($this->getAttribute('contentPosition')) {
+            case 'top':
+                $contentPosition = 'start';
+                break;
+
+            case 'bottom':
+                $contentPosition = 'end';
+                break;
+
+            default:
+                $contentPosition = 'center';
+                break;
+        }
+
+        if ($this->getAttribute('openVideoInPopup')) {
+            $openVideoInPopup = $this->getAttribute('openVideoInPopup');
+        }
+
+        $Engine->assign([
+            'this'                      => $this,
+            'backgroundColor'           => $backgroundColor,
+            'autoplay'                  => $autoplay,
+            'muted'                     => $muted,
+            'loop'                      => $loop,
+            'playsinline'               => $playsinline,
+            'backgroundVideoBrightness' => $backgroundVideoBrightness,
+            'fontColor'                 => $fontColor,
+            'contentMaxWidth'           => $contentMaxWidth,
+            'contentPosition'           => $contentPosition,
+            'openVideoInPopup'          => $openVideoInPopup,
+        ]);
+
+        return $Engine->fetch(dirname(__FILE__).'/BackgroundVideo.html');
+    }
+}
